434585d0e24cf29da2d28c89c929485c06e9a569
locbh <locbh3010@gmail.com>
2025-03-30 18:19:13 +0700

    @portfolio:fix build

--- src/shared/components/followCursor/Cursor.tsx
+++ src/shared/components/followCursor/Cursor.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { motion } from 'framer-motion'
-import type { FollowCursorProps } from './types'
+import { motion } from 'motion/react'
 import { DEFAULT_SCALE, HALF_FOLLOW_CURSOR_SIZE, HOVER_SCALE } from './constants'
+import type { FollowCursorProps } from './types'
 
 export default function Cursor({ mousePosition, isHovering }: FollowCursorProps) {
     return (
